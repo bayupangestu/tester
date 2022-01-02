6a9fc32e3d48b77ade37b6632b775a2f872bba96
bayupangestu <bayupangestu21@gmail.com>
2022-01-02 23:30:50 +0700

    first commit

---
+++ mumbling.js
@@ -0,0 +1,11 @@
+var string = "ZpglnRxqenU";
+
+function accum(s) {
+    var value = 0;
+    var upper = s.toUpperCase();
+    var char = upper.split('');
+    var result = reduce()
+    return result;
+}
+
+console.log(accum(string));
